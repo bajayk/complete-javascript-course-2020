ca51befeedc311bcebc8f65c3a4726e5c967ec95
Ajay Badgujar <bajayk@gmail.com>
2020-08-30 18:26:29 +0530

    Data Types and Variables

---
+++ 003 Data types and Variables/script.js
@@ -0,0 +1,21 @@
+var name = 'Ajay';
+//console.log(name);
+
+var standard = '5th';
+console.log(typeof(standard));
+standard = 5;
+console.log(typeof(standard));
+
+var age = 10;
+
+var isPresent = false;
+//console.log(isPresent);
+
+var numberOfSubjects;
+//console.log(numberOfSubjects);
+
+var _person = 1;
+console.log(_person);
+
+var function = 1
+
