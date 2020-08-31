621a6078320c654bac145d354e423cbbd8bb2b26
Ajay Badgujar <bajayk@gmail.com>
2020-08-31 13:48:03 +0530

    Type Coercion

---
+++ 004 Type Coercion or Type Conversion/script.js
@@ -0,0 +1,11 @@
+var fname = 'Vishal';
+var lname = 'Sharam';
+var age = 31;
+
+console.log(fname + ' ' + lname + ' is a ' + age + ' year old guy.');
+
+var isMarried = true;
+
+console.log(fname + ' ' + lname + ' is a ' + age + ' year old guy. And is he married? ' + isMarried);
+
+
