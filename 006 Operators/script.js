80e4d89989637ca5521bf83b9e187f51181a1dc9
Ajay Badgujar <bajayk@gmail.com>
2020-09-01 16:59:44 +0530

    JavaScript Operators

---
+++ 006 Operators/script.js
@@ -0,0 +1,160 @@
+/*
+* ---------------------------------
+* Assignment Operators
+* ---------------------------------
+*/ 
+/*
+// (=) operator assigns the value to variable
+
+var name = "Ramchandra";
+console.log(name);
+
+// (+=) operator adds a value to a variable
+
+var age = 34;
+console.log(age);
+age += 5;
+console.log(age);
+
+// (-=) operator subtracts a value from a variable.
+
+var total = 200;
+total -= 10;
+console.log(total);
+
+
+// (*=) operator multiplies a variable.
+
+var numberofstudents = 30;
+numberofstudents *= 2;
+console.log(numberofstudents);
+
+// (/=) operator divides a variable.
+
+var chocolates = 50;
+chocolates /= 5;
+console.log(chocolates);
+
+// (%=) operator assigns a remainder to a variable.
+
+chocolates = 52;
+chocolates %= 5;
+console.log(chocolates);
+*/
+
+/*
+* ---------------------------------
+* Arithmetic Operators
+* ---------------------------------
+*/ 
+/*
+// The addition operator (+) adds numbers.
+
+var total = 4 + 5;
+
+console.log(total);
+
+var fname = "Mahesh";
+var lname = "Vanara";
+
+var fullName = fname + ' ' + lname;
+
+console.log(fullName);
+
+console.log(fname + 5);
+
+console.log('5' + 5);
+
+// The subtraction operator (-) subtracts numbers.
+
+var result = 320;
+var mathsMarks = 34;
+
+console.log(result - mathsMarks);
+
+
+// The multiplication operator (*) multiplies numbers.
+
+console.log( 5 * 3);
+
+// The division operator (/) divides numbers.
+
+console.log( 45 / 9);
+
+// The modulus operator (%) returns the division remainder.
+
+console.log( 47 % 9);
+
+// The increment operator (++) increments numbers.
+
+var bananas = 12;
+console.log(bananas);
+bananas++;
+console.log(bananas);
+
+// The decrement operator (--) decrements numbers.
+
+bananas--;
+console.log(bananas);
+*/
+/*
+* ---------------------------------
+* Comparison Operators
+* ---------------------------------
+*/ 
+/*
+// == equal to
+
+console.log( '2' == 2);
+
+// === equal value and equal type
+
+console.log('2' === 2);
+
+// != not equal
+
+console.log( 2 != '2');
+
+// !== not equal value or not equal type
+
+console.log( 2 !== '2');
+
+// > greater than
+
+console.log( 3 > 10);
+
+// < less than
+
+console.log( 3 < 3);
+
+// >= greater than or equal to
+
+console.log( 3 >= 4);
+
+// <= less than or equal to
+
+console.log( 3 <= 2);
+*/
+
+
+/*
+* ---------------------------------
+* Logical Operators
+* ---------------------------------
+*/ 
+
+// &&	logical and
+
+var x = 1;
+var y = 2;
+var z = 3
+
+console.log(2 == y && x == 1);
+
+// ||	logical or
+
+console.log(4 == y || x == 5);
+
+// !	logical not
+
+console.log(!false);
